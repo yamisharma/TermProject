65d640b030dbe4971e40a82b5eb3bc9037d4b578
bryce-jacobs <47409450+bryce-jacobs@users.noreply.github.com>
2020-03-07 14:02:56 -0800

    Add files via upload

---
+++ ratingsProbFit.R
@@ -0,0 +1,23 @@
+ratingProbsFit <- function(dataIn,maxRating,predMethod,embedMeans,specialArgs) {
+  #dataIn:
+  
+    # put input validation code here
+      # if fails, correct it and then print msg saying we did
+  
+  
+  # predMethod & embedMeans:
+  if () {  # check for predMethod
+    if () {  # check if need to use embedMeans
+      
+    }
+    
+  }
+  
+  
+  # specialArgs (how?)
+}
+
+
+predict <- function(probsFitOut,newXs) {
+  # put input validation
+}
