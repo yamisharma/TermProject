945634bf5e275d25aba9e84577acdd38814b5cfd
Megana Bobba <51874806+meganabyte@users.noreply.github.com>
2020-03-10 17:32:13 -0700

    Update ratingsProbFit.R

--- ratingsProbFit.R
+++ ratingsProbFit.R
@@ -1,10 +1,23 @@
+library(lme4)
+library(partykit)
+data(InstEval)
+ie <- InstEval
+ie <- ie[,c(1:2,7)]
+names(ie) <- c("userId", "itemId", "rating")
+
+song <- read.csv(file="songsDataset.csv")
+names(song) <- c("userId", "itemId", "rating")
+
 ratingProbsFit <- function(dataIn,maxRating,predMethod,embedMeans,specialArgs) {
+  
   #dataIn:
   
   # put input validation code here
   # if fails, correct it and then print msg saying we did
   
   # split into train and test set
+  
+  
   # predMethod & embedMeans:
   if (predMethod == 'logit') {  # check for predMethod
     if (embedMeans) {  # check if need to use embedMeans
@@ -34,13 +47,6 @@ ratingProbsFit <- function(dataIn,maxRating,predMethod,embedMeans,specialArgs) {
 
 
 
-
-
-predict <- function(probsFitOut,newXs) {
-  # put input validation
-}
-
-
 nmfFunc <- function(ratingsIn, testSet, rnk) {
   require(recosystem)
   r <- Reco()
@@ -48,5 +54,17 @@ nmfFunc <- function(ratingsIn, testSet, rnk) {
   testset <- data_memory(testSet[,1],testSet[,2],testSet[,3],index1=TRUE)
   r$train(dataset, opts=list(dim=rnk, nmf=TRUE))
   result <- r$output(out_memory(),out_memory())
+  
   preds <- r$predict(testset, out_memory())
+  
+}
+
+
+cartFunc <- function(ratingsIn, testSet) {
+  #ctout <- ctree(ratingsIn ~., data=mlb)
+  #pred <- predict(ctout, dataToPredict, type="prob")
+}
+
+predict <- function(probsFitOut,newXs) {
+  # put input validation
 }
