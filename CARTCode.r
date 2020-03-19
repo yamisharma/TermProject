c51e9607e98bc40bfbe4598474ff283642ebe1ad
Yamini Sharma <ysharma@ucdavis.edu>
2020-03-19 07:05:38 -0700

    Update CARTCode.r

--- CARTCode.r
+++ CARTCode.r
@@ -53,7 +53,9 @@ nmfFunc <- function(ratingsIn, testSet, rnk) {
 cartFunc <- function(ratingsIn, testSet) {
 
   #View(ratingsIn)
-  
+  ratingsIn$userId <- as.factor(ratingsIn$userId)
+  ratingsIn$itemId <- as.factor(ratingsIn$itemId)
+
   userMeans <- tapply(ratingsIn$rating, ratingsIn$userId, mean)
   itemMeans <- tapply(ratingsIn$rating, ratingsIn$itemId, mean)
   ratingsInEmb <- ratingsIn
@@ -64,7 +66,7 @@ cartFunc <- function(ratingsIn, testSet) {
   ratingsInEmb$userId <- as.vector(ratingsInEmb$userId)
   ratingsInEmb$itemId <- as.vector(ratingsInEmb$itemId)
 
-  ctout <- ctree(rating ~ ., data = ratingsInEmb)
+  ctout <- ctree(rating ~ ., data = ratingsInEmb (#maybe have the training set here @aditi) not sure. Do we do CV on the emb dataset to create a new train set??)
   print(node_party(ctout))
 }
 
@@ -95,3 +97,21 @@ trainSong <- song[-testSetIndices, ]
 tstSong <- song[testSetIndices, ]
 
 ratingProbsFit(trainIE, 5, 'CART', TRUE, 4)
+    
+ ####
+ elseif(probsFitOut == 'CART')
+  #replace ratings In with newXS --- newXS is basically the test set from emb in the org cart func above
+  ratingsIn$userId <- as.factor(ratingsIn$userId)
+  ratingsIn$itemId <- as.factor(ratingsIn$itemId)
+
+  userMeans <- tapply(ratingsIn$rating, ratingsIn$userId, mean)
+  itemMeans <- tapply(ratingsIn$rating, ratingsIn$itemId, mean)
+  ratingsInEmb <- ratingsIn #replce again
+
+  ratingsInEmb$userId <- userMeans[ratingsIn$userId]
+  ratingsInEmb$itemId <- itemMeans[ratingsIn$itemId]
+
+  ratingsInEmb$userId <- as.vector(ratingsInEmb$userId)
+  ratingsInEmb$itemId <- as.vector(ratingsInEmb$itemId)
+#use in built predict function and take test set from above + the tree that CART spits out as args. Divide this pred input with test set (well not the set but maybe take the abs value or sthg) and assign that as a probability variable
+#then return the latest pred
