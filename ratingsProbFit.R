6819677d76d4dff0c443ad060885ab33ed4dedf3
Megana Bobba <51874806+meganabyte@users.noreply.github.com>
2020-03-19 10:33:28 -0700

    Update ratingsProbFit.R

--- ratingsProbFit.R
+++ ratingsProbFit.R
@@ -1,70 +1,33 @@
-library(lme4)
-library(partykit)
-data(InstEval)
-ie <- InstEval
-ie <- ie[,c(1:2,7)]
-names(ie) <- c("userId", "itemId", "rating")
 
-song <- read.csv(file="songsDataset.csv")
-names(song) <- c("userId", "itemId", "rating")
 
 ratingProbsFit <- function(dataIn,maxRating,predMethod,embedMeans,specialArgs) {
-  
-  #dataIn:
-  
-  # put input validation code here
-  # if fails, correct it and then print msg saying we did
-  
-  # split into train and test set
-  
-  
-  # predMethod & embedMeans:
-  if (predMethod == 'logit') {  # check for predMethod
-    if (embedMeans) {  # check if need to use embedMeans
+  if (predMethod == 'logit') {  
+    if (embedMeans) {  
       
     }
   } 
   
-  if (predMethod == 'NMF') {  # check for predMethod
+  if (predMethod == 'NMF') { 
     
   }
   
-  if (predMethod == 'kNN') {  # check for predMethod
-    if (embedMeans) {  # check if need to use embedMeans
+  if (predMethod == 'kNN') {  
+    if (embedMeans) { 
       
     }
   }
   
-  if (predMethod == 'CART') {  # check for predMethod
-    if (embedMeans) {  # check if need to use embedMeans
+  if (predMethod == 'CART') {  
+    if (embedMeans) { 
       
     }
   }
   
-  
-  # specialArgs (how?)
-}
-
-
-
-nmfFunc <- function(ratingsIn, testSet, rnk) {
-  require(recosystem)
-  r <- Reco()
-  dataset <- data_memory(ratingsIn[,1],ratingsIn[,2],ratingsIn[,3],index1=TRUE)
-  testset <- data_memory(testSet[,1],testSet[,2],testSet[,3],index1=TRUE)
-  r$train(dataset, opts=list(dim=rnk, nmf=TRUE))
-  result <- r$output(out_memory(),out_memory())
-  
-  preds <- r$predict(testset, out_memory())
-  
-}
-
-
-cartFunc <- function(ratingsIn, testSet) {
-  #ctout <- ctree(ratingsIn ~., data=mlb)
-  #pred <- predict(ctout, dataToPredict, type="prob")
 }
 
 predict <- function(probsFitOut,newXs) {
-  # put input validation
+  
+  
+  
+  
 }
